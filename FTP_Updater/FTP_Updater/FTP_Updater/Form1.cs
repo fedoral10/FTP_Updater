252fc81dc7dcb4d71bad56472842598de3eb28b5
Jorge Potosme <fedoral10@gmail.com>
2013-09-26 12:11:33 -0600

    clases de encriptacion y para el ftp

---
+++ FTP_Updater/FTP_Updater/FTP_Updater/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+namespace FTP_Updater
+{
+    public partial class Form1 : Form
+    {
+        ftp ftpClient;
+        public Form1()
+        {
+            InitializeComponent();
+            xml.inicializa_parametros();   
+            if (xml.debug == true)
+            {
+                ViewConsole(xml.debug);
+            }
+            Console.WriteLine("usuario:"+xml.usuario);
+            Console.WriteLine("pass:" + xml.password);
+            Console.WriteLine("servidor:" + xml.servidor);
+            Console.WriteLine("debug:" + xml.debug);
+            Console.WriteLine("cantidad_archivos:" + xml.lista_archivos.Count);
+            ftpClient = new ftp(@"ftp://" + xml.servidor, xml.usuario, encrypter.Desencriptar(xml.password));
+            //actualiza_archivo("archivo1.txt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //actualiza_lista_archivos(xml.lista_archivos);
+            //Console.WriteLine("123456 encriptado:"+encrypter.Encriptar("123456"));
+            //Console.WriteLine("123456 desencriptado:"+encrypter.Desencriptar(encrypter.Encriptar("123456")));
+        }
+
+        private void actualiza_archivo(string archivo)
+        {
+            DateTime d= File.GetLastWriteTime(archivo);
+            DateTime xl = ftpClient.getFileCreatedDateTime(archivo);
+            if (d < xl)
+            {
+                File.Delete(archivo);
+                ftpClient.download(archivo, archivo);
+            }
+        }
+
+        private void actualiza_lista_archivos(List<string> archivos)
+        {
+            foreach (string arch in archivos)
+            {
+                actualiza_archivo(arch);
+            }
+        }
+
+        private void ViewConsole(bool valor)
+        {
+            if (valor)
+                Win32.AllocConsole();
+            else
+                Win32.FreeConsole();
+         }
+    }
+
+    public class Win32
+    {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+    }
+}
+ 
+ 
+    
---
+++ FTP_Updater/FTP_Updater/FTP_Updater/xml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+namespace FTP_Updater
+{
+    static class xml
+    {
+        public static string usuario = "";
+        public static string password = "";
+        public static string servidor = "";
+        public static List<string> lista_archivos = new List<string>();
+        public static bool debug;
+        public static string inicializa_parametros()
+        {
+            XmlTextReader reader = new XmlTextReader("configuracion.xml");
+            string valor = "";
+            while (reader.Read())
+            {
+             switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        /*Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");*/
+                        try
+                        {
+                            if (reader.Name.ToUpper() == "USUARIO")
+                            {
+                                usuario = reader.ReadElementContentAsString();
+                            }
+                            if (reader.Name.ToUpper() == "CONTRASENIA")
+                            {
+                                password = reader.ReadElementContentAsString();
+                            }
+                            if (reader.Name.ToUpper() == "SERVIDOR")
+                            {
+                                servidor = reader.ReadElementContentAsString();
+                            }
+                            if (reader.Name.ToUpper() == "DEBUG")
+                            {
+                                string val = reader.ReadElementContentAsString();
+                                if (val == "S")
+                                {
+                                    debug = true;
+                                }
+                                else
+                                {
+                                    debug = false;
+                                }
+
+                            }
+                            if (reader.Name.ToUpper() == "LISTA_ARCHIVOS")
+                            {
+                                string archivos = reader.ReadElementContentAsString();
+                                string palabra="";
+                                int c=0;
+                                foreach(char x in archivos.ToCharArray())
+                                {
+                                    palabra += x;
+                                    if (x == '\n')
+                                    {
+                                        
+                                        if (!string.IsNullOrEmpty(palabra.Trim()))
+                                        {
+                                            lista_archivos.Add(palabra.Trim());
+                                            palabra = "";
+                                            c++;
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.Write(ex.Message);
+                        }
+                        //Console.Write(reader.Value);
+                        break;
+                    /*case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;*/
+                    /*case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;*/
+                }
+            }
+            return valor;
+        }
+    }
+}
